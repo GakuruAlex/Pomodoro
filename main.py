fbdc19883671f15513f22c90f8fb5def1e25d8af
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-13 12:49:23 +0000

    initialize and start the countdown on pressing start button

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import Canvas, PhotoImage, Tk, Label, Button
+from pomodoro import Pomodoro
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -11,6 +12,7 @@ TICK = "✓"
 
 def main()-> None:
     window = Tk()
+    
     window.title("Pomodoro")
     window.config(padx=100, pady=50, bg=YELLOW)
     timer = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
@@ -18,10 +20,10 @@ def main()-> None:
     canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness= 0)
     tomato_img =PhotoImage(file="tomato.png")
     canvas.create_image(100,112 ,image=tomato_img )
-    canvas.create_text(120, 125, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+    timer_text = canvas.create_text(120, 125, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
     canvas.grid(row=1, column=2)
-
-    start = Button(text="Start", font=(FONT_NAME, 14, "bold"), highlightthickness=0, bg=YELLOW, borderwidth=0)
+    pomodoros = Pomodoro(canvas=canvas, window=window, timer_text=timer_text)
+    start = Button(text="Start", font=(FONT_NAME, 14, "bold"), highlightthickness=0, bg=YELLOW, borderwidth=0, command=pomodoros.start_timer)
 
     start.grid(row=3, column=1)
     reset = Button(text="Reset", font=(FONT_NAME, 14, "bold"), highlightthickness=0, bg=YELLOW, borderwidth=0)
