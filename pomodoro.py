e54d7ee63f39a359749f05441bcb56fe063025e0
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-13 12:48:39 +0000

    create a counter and countdown after every second, change the counter text each second

--- pomodoro.py
+++ pomodoro.py
@@ -5,4 +5,9 @@ class Pomodoro:
         self.timer_text = timer_text
         self.window = window
    
-    
+    def count_timer(self, count: int):
+        self.canvas.itemconfig(self.timer_text, text=count )
+        if count > 0:
+            self.window.after(1000, self.count_timer, count - 1)
+    def start_timer(self):
+        self.count_timer(5)
