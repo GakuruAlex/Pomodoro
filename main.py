bc5f78bad2f0280d66342260f3a7c0fba8a150e8
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-13 12:04:21 +0000

    create buttons for start and reset and also a checkmark

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Canvas, PhotoImage, Tk
+from tkinter import Canvas, PhotoImage, Tk, Label, Button
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -7,16 +7,28 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+TICK = "✓"
 
 def main()-> None:
     window = Tk()
     window.title("Pomodoro")
     window.config(padx=100, pady=50, bg=YELLOW)
+    timer = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
+    timer.grid(row=0, column=2)
     canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness= 0)
     tomato_img =PhotoImage(file="tomato.png")
     canvas.create_image(100,112 ,image=tomato_img )
     canvas.create_text(120, 125, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
-    canvas.pack()
+    canvas.grid(row=1, column=2)
+
+    start = Button(text="Start", font=(FONT_NAME, 14, "bold"), highlightthickness=0, bg=YELLOW, borderwidth=0)
+
+    start.grid(row=3, column=1)
+    reset = Button(text="Reset", font=(FONT_NAME, 14, "bold"), highlightthickness=0, bg=YELLOW, borderwidth=0)
+    reset.grid(row=3, column=3)
+    
+    check_mark = Label(text= TICK,fg=GREEN, bg=YELLOW, font=("DejaVu Sans",25))
+    check_mark.grid(row = 4, column= 1)
 
     window.mainloop()
 
