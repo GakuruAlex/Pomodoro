dfcf7d61ab15af8c0ed2b3110e8156b4de8e4f1f
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-13 16:28:09 +0300

    display the time in minutes and seconds

--- pomodoro.py
+++ pomodoro.py
@@ -1,4 +1,4 @@
-
+from time import strftime, gmtime
 class Pomodoro:
     def __init__(self, canvas, timer_text, window):
         self.canvas = canvas
@@ -6,8 +6,9 @@ class Pomodoro:
         self.window = window
    
     def count_timer(self, count: int):
-        self.canvas.itemconfig(self.timer_text, text=count )
+        count_text = strftime("%M:%S", gmtime(count))
+        self.canvas.itemconfig(self.timer_text, text=count_text )
         if count > 0:
             self.window.after(1000, self.count_timer, count - 1)
     def start_timer(self):
-        self.count_timer(5)
+        self.count_timer(300)
